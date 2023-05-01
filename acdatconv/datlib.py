f53361009058c5357965802faf954d744deaba47
s-yagyu <71371237+s-yagyu@users.noreply.github.com>
2023-05-01 22:31:35 +0900

    Update datlib.py

--- acdatconv/datlib.py
+++ acdatconv/datlib.py
@@ -5,7 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import openpyxl
 import pandas as pd
-from pandas.io.json import json_normalize
+
+# from pandas.io.json import json_normalize
+import pandas.json_normalize as json_normalize
 
 from acdatconv import datconv as dv
 
